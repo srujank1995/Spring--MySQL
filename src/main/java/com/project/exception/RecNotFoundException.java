7372f5f15181033470e8fee701f33a3d7ed61c84
sru_kinj1995 <srujankinjawadekar1@gmail.com>
2024-05-02 23:11:10 +0530

    exception function update

--- src/main/java/com/project/exception/RecNotFoundException.java
+++ src/main/java/com/project/exception/RecNotFoundException.java
@@ -4,6 +4,7 @@ package com.project.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@SuppressWarnings("serial")
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class RecNotFoundException extends RuntimeException {
 
